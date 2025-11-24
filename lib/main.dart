c4d869efe1736480d127ebf70e8733189fccceb7
catalina1998 <110789682+catalina1998@users.noreply.github.com>
2025-11-24 14:10:54 -0300

    Actualizacion del scroll

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-const pokemonApiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=1500';
-const pokemonImageBaseUrl =
-    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
-const frontImageExtension = '.png';
+const pokemonApiUrl = 'https://pokeapi.co/api/v2/pokemon'; 
 
 void main() {
   runApp(PokeDexApp());
@@ -16,7 +13,10 @@ class PokeDexApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Mi pokédex interactiva', home: PokemonList());
+    return MaterialApp(
+      title: 'Mi pokédex interactiva',
+      home: PokemonList(),
+    );
   }
 }
 
@@ -30,64 +30,110 @@ class PokemonList extends StatefulWidget {
 class _PokemonListState extends State<PokemonList> {
   List<Map<String, dynamic>> pokemonMapList = [];
 
+  bool isLoading = false;
+  int offset = 0;
+  final int limit = 20;
+  final ScrollController _scrollController = ScrollController();
+
   @override
   void initState() {
     super.initState();
     getPokemonList();
+
+    // Listener para detectar el fin de la lista
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels >=
+              _scrollController.position.maxScrollExtent - 200 &&
+          !isLoading) {
+        getPokemonList();
+      }
+    });
   }
 
   Future<void> getPokemonList() async {
-    final response = await http.get(Uri.parse(pokemonApiUrl));
+    // Tope de seguridad de 1500 
+    if (isLoading || offset >= 1500) return;
 
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      final List results = data['results'];
+    setState(() {
+      isLoading = true;
+    });
 
-      List<Map<String, dynamic>> retrievedPokemons = [];
+    final url = Uri.parse('$pokemonApiUrl?limit=$limit&offset=$offset');
+    
+    try {
+      final response = await http.get(url);
 
-      var id = 1;
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        final List results = data['results'];
 
-      for (var pokemon in results) {
-        final pokemonName = pokemon['name'];
-        final pokemonId = id;
-        final imageUrl = '$pokemonImageBaseUrl$id$frontImageExtension';
+        List<Map<String, dynamic>> newPokemons = [];
 
-        retrievedPokemons.add({
-          'id': pokemonId,
-          'name': pokemonName,
-          'image': imageUrl,
+        for (var i = 0; i < results.length; i++) {
+          int realId = offset + i + 1;
+          final imageUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$realId.png';
+
+          newPokemons.add({
+            'id': realId,
+            'name': results[i]['name'],
+            'image': imageUrl,
+          });
+        }
+
+        setState(() {
+          pokemonMapList.addAll(newPokemons);
+          offset += limit;
+          isLoading = false;
         });
-
-        id++;
+      } else {
+        throw Exception('Error al cargar');
       }
-
+    } catch (e) {
       setState(() {
-        pokemonMapList = retrievedPokemons;
+        isLoading = false;
       });
-    } else {
-      throw Exception('Failed to retrieve data');
+      print("Error: $e");
     }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Listado de Pokémones"),),
-      body: pokemonMapList.isEmpty
-          ? Center(child: CircularProgressIndicator())
-          : Scrollbar(child: ListView.builder(
-              itemCount: pokemonMapList.length,
+      appBar: AppBar(title: Text("Listado de Pokémones")),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              controller: _scrollController,
+              itemCount: pokemonMapList.length + 1,
               itemBuilder: (context, index) {
+                if (index == pokemonMapList.length) {
+                  return isLoading
+                      ? Center(child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: CircularProgressIndicator(),
+                        ))
+                      : SizedBox.shrink();
+                }
+
                 final pokemon = pokemonMapList[index];
+                
+                // Diseño básico temporal (Catalina lo mejorará después)
                 return ListTile(
                   leading: Image.network(pokemon['image']),
-                  title: Text(
-                    pokemon['name'].toString(),
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
+                  title: Text(pokemon['name'].toString()),
                 );
               },
-            ),)
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
